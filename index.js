e2e45f7f7cda464fde6bd221fd0f7532e40a257e
IhorHudkov <gudkov.igor@gmail.com>
2021-01-26 17:22:13 +0300

    Add saving the form data to the database (IndexedDB)

--- index.js
+++ index.js
@@ -1,16 +1,72 @@
-import Event from "./event";
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-console */
+/* eslint-disable prefer-const */
+/* eslint-disable func-names */
 
 const form = document.forms.eventForm;
+const nameInput = form.name;
+const participantsInput = form.participants;
+const dayInput = form.day;
+const timeInput = form.time;
 
-function AddEvent(e) {
-  const name = form.name.value;
-  const participants = form.participants.value;
-  const day = form.day.value;
-  const time = form.time.value;
-  const event = new Event(name, participants, day, time);
-  e.preventDefault();
-  // eslint-disable-next-line no-alert
-  alert(event.name);
-  document.location.href = "index.html";
-}
-form.onsubmit = AddEvent;
+let db;
+
+window.onload = function () {
+  let openRequest = indexedDB.open("calendar", 1);
+
+  openRequest.onupgradeneeded = function () {
+    db = openRequest.result;
+    let eventStore = db.createObjectStore("events", {
+      keyPath: "id",
+      autoIncrement: true,
+    });
+    eventStore.createIndex("name", "name", { unique: false });
+    eventStore.createIndex("participants", "participants", { unique: false });
+    eventStore.createIndex("dayTime", "dayTime", { unique: true });
+    console.log("Database setup complete");
+  };
+
+  openRequest.onerror = function () {
+    console.error("Error", openRequest.error);
+  };
+
+  openRequest.onsuccess = function () {
+    console.log("Database opened succesfully");
+    db = openRequest.result;
+  };
+
+  form.onsubmit = (e) => {
+    e.preventDefault();
+
+    let event = {
+      name: nameInput.value,
+      participants: participantsInput.value,
+      dayTime: dayInput.value + timeInput.value,
+    };
+
+    let transaction = db.transaction(["events"], "readwrite");
+
+    let eventStore = transaction.objectStore("events");
+
+    let addRequest = eventStore.add(event);
+    addRequest.onsuccess = function () {
+      nameInput.value = "";
+      participantsInput.value = "Choose...";
+      dayInput.value = "Choose...";
+      timeInput.value = "Choose...";
+    };
+
+    transaction.oncomplete = function () {
+      console.log("Transaction completed: database modification finished.");
+      displayCalendar();
+    };
+
+    transaction.onerror = function () {
+      console.log("Transaction not opened due to error");
+    };
+  };
+
+  function displayCalendar() {
+    console.log("Ok!");
+  }
+};
